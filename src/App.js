2213fc429e07f28ff0a916c349cc802e98eba0bf
Precious Gartrell <precious.gartrell@allovue.com>
2024-02-01 14:03:34 -0500

    redo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav>
+      <img className="reactLogo" src="/logo512.png" alt=""/>
+      <div className="maindiv">        
+          <div>
+            <h1 className="h1Title">Fun facts about Me</h1>
+          </div>
+          <div>
+              <ul className = "list">
+                  <li>Love to dance.</li>
+                  <li>Love to sing</li>
+                  <li>Love to hang with family and friends</li>
+                  <li>Love my Hubby</li>
+              </ul>
+            </div>
+        </div>
+    </nav>
   );
 }
 
 export default App;
+
